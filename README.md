5c6235d6446b80a47abdc57f51a50c727e2ad682
Mathias Buus <mathiasbuus@gmail.com>
2019-09-05 14:07:32 +0200

    add exists and rollback

--- README.md
+++ README.md
@@ -11,13 +11,28 @@ var S3Snapshot = require('s3-snapshot')
 
 ## API
 
-### `const snap = S3Snapshot(directory, s3)`
+### `const snap = S3Snapshot(directory, s3path, s3)`
 
 Create new instance that will snapshot `directory` using a `s3-storage` instance
 
-### `snap.download(key, cb)`
+### `snap.exists(cb)`
+
+Check if a snapshot exists
+
+### `snap.download(cb)`
+
+Download a snapshot
+
+### `snap.rollback(cb)`
+
+Rollback to a previous version of a snapshot. `cb(null, true)`
+if a previous version exists and `cb(null, false)` otherwise.
+
+You have to redownload it afterwards
 
 ### `snap.snapshot(cb)`
+
+Make a new snapshot. Resets any previous rollback.
 
 ## Install
 
--- index.js
+++ index.js
@@ -5,18 +5,44 @@ const rimraf = require('rimraf')
 const low = require('last-one-wins')
 
 module.exports = class Snapshot {
-  constructor (dir, s3) {
+  constructor (dir, key, s3) {
     this.dir = dir
     this.s3 = s3
+    this.key = key
+    this.version = null
 
     // effectively mutex
-    this.download = low(this._download.bind(this))
-    this.snapshot = low(this._snapshot.bind(this))
+    this.download = low(this._download.bind(this)).bind(this, this.key)
+    this.snapshot = low(this._snapshot.bind(this)).bind(this, this.key)
+  }
+
+  rollback (cb) {
+    this.s3.versions(this.key, (err, list) => {
+      if (err) return cb(err)
+
+      let next = false
+
+      for (const { version } of list) {
+        if (next) {
+          this.version = version
+          return cb(null, true)
+        }
+        if (version === this.version || !this.version) {
+          next = true
+        }
+      }
+
+      cb(null, false)
+    })
+  }
+
+  exists (cb) {
+    this.s3.exists(this.key, { version: this.version }, cb)
   }
 
   _download (key, cb) {
     const tmp = this.dir + '-tmp'
-    pump(this.s3.createReadStream(key), tar.extract(tmp), err => {
+    pump(this.s3.createReadStream(key, { version: this.version }), tar.extract(tmp), err => {
       if (err) return cb(err)
       rimraf(this.dir, (err) => {
         if (err) return cb(err)
@@ -26,7 +52,7 @@ module.exports = class Snapshot {
     })
   }
 
-  _snapshot (cb) {
+  _snapshot (key, cb) {
     const tarball = this.dir + '.tar'
 
     pump(tar.pack(this.dir), fs.createWriteStream(tarball), err => {
@@ -35,9 +61,13 @@ module.exports = class Snapshot {
       fs.stat(tarball, (err, stat) => {
         if (err) return cb(err)
 
-        pump(fs.createReadStream(tarball), this.s3.createWriteStream(tarball, {
+        pump(fs.createReadStream(tarball), this.s3.createWriteStream(key, {
           length: stat.size
-        }), cb)
+        }), (err) => {
+          if (err) return cb(err)
+          this.version = null
+          cb(null)
+        })
       })
     })
   }
